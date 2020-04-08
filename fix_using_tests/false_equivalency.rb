6a6f54848ba293f8e4e63bbc0bba1c5ff7600bb1
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-08 11:44:00 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -9,12 +9,11 @@ def prompt_user
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
-
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  if num == 2
      "HAM HAM HAM IN MY TUMMY"
+  elsif num == 1
+     "YUM YUM MUNCH MUNCH MUNCH"
   end
 end
 
